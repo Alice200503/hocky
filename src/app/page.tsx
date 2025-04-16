e28b84f9a63fa77e1214ad9bb147e9073dee4d27
Alice200503 <ibuyao@gmail.com>
2025-04-16 11:30:21 +0800

    Fix routing configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect('/en')
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-bold">Adam Zhao</h1>
+        <h2 className="text-2xl">#19 | Les Seigneurs des Mille-Iles</h2>
+      </div>
+    </main>
+  )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   locales: ['en', 'fr'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localePrefix: 'always'
 });
  
 export const config = {
